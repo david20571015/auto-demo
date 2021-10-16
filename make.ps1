8027135263758a6a49931b91a79d8ce9ab10b188
david20571015 <david20571015@gmail.com>
2021-10-17 05:03:29 +0800

    Update: it is unnecessary to generate build_test.exe

--- make.ps1
+++ make.ps1
@@ -1,7 +1,4 @@
-python -OO -m PyInstaller --onefile .\build_test.py
-.\dist\build_test.exe
 python -OO -m PyInstaller --onefile --add-data "test.json;." .\grade.py
 
 rmdir -Recurse -Force ".\build" 
-rm ".\build_test.spec" 
 rm ".\grade.spec"
