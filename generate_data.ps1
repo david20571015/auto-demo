2081fd14016f5fe2aaa018311c97c138ba6861f6
konoha26435 <78638739+konoha26435@users.noreply.github.com>
2021-10-18 06:00:55 +0800

    Add files via upload

---
+++ generate_data.ps1
@@ -0,0 +1,30 @@
+$data = 
+    "123.456`r`n",
+    "2`r`n",
+    "7 5`r`n",
+    "456.789`r`n",
+    "2.357 6.819`r`n",
+    "88010000`r`n",
+    "33`r`n",
+    "5 5 5 5 5`r`n",
+    "6 7 8 9 8 10`r`n"
+$in_file = "1.in"
+$out_file = "1.out"
+$dir = "test\"
+
+$path = ".\" + $dir
+if (!(test-path $path)) {
+    New-Item -ItemType Directory -Force -Path $path
+}
+
+for ($i = 1; $i -le $data.count; $i++) {
+    $p = $path + $i.ToString() + "\"
+    New-Item -ItemType Directory -Force -Path $p
+    New-Item -ItemType File -Force -Path $p -Name $in_file -Value $data[$i-1]
+    
+    $exe_file = ".\" + $i.ToString() + ".exe"
+    if(test-path $exe_file){
+        get-content ($p+$in_file) | &$exe_file > ($p+$out_file)
+    }
+}
+# get-content .\test\1\1.in | .\1.exe > .\test\1\1.out
