b4ab8f290138ce548f0f70cb3f2f97d6f2201bb2
David Chiu <david20571015@gmail.com>
2023-03-02 02:43:52 +0800

    chore: disable pylitn

--- grade.py
+++ grade.py
@@ -14,7 +14,7 @@ def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
+        base_path = sys._MEIPASS  # type: ignore
     except Exception:
         base_path = Path.cwd()
 
